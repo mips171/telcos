d653e629abc71c7969e7f3bdf72695ec08f1a592
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2019-09-14 23:52:29 +1000

    ltestatus: add band and cell information

--- private_feeds/luci-app-ltestatus/luasrc/model/cbi/ltestatus.lua
+++ private_feeds/luci-app-ltestatus/luasrc/model/cbi/ltestatus.lua
@@ -21,9 +21,9 @@ if has_mmcli then
         local sp = ": "
 	local lanIPNumber = luci.sys.exec("uci get network.lan.ipaddr")
 	local modemstatus   = luci.sys.exec("mmcli -m 0 > /tmp/modemstatus")
-        
+
         function checkInternet()
-                luci.sys.exec("while ! ping -c1 www.telcoantennas.com.au >/dev/null; do echo '<font color='red'>disconnected</font>. If signal is good, please check that the correct APN and other mobile data settings are in use.' > /tmp/internetstatus && exit 0; done ; echo '<font color='#20BF55'>connected</font>' > /tmp/internetstatus && exit 0")   
+                luci.sys.exec("while ! ping -c1 www.telcoantennas.com.au >/dev/null; do echo '<font color='red'>disconnected</font>. If signal is good, please check that the correct APN and other mobile data settings are in use.' > /tmp/internetstatus && exit 0; done ; echo '<font color='#20BF55'>connected</font>' > /tmp/internetstatus && exit 0")
                 return tostring(luci.sys.exec("cat /tmp/internetstatus"))
         end
 
@@ -45,11 +45,32 @@ if has_mmcli then
 	local mobile_percentage = luci.sys.exec("grep quality /tmp/modemstatus | sed 's/.*: //' | tr -cd [:digit:] ")
 
         local sim_missing   = luci.sys.exec("grep 'sim-missing' /tmp/modemstatus | sed 's/.*: //' | tr -d \"'\"")
-        
+
+        local ab = luci.sys.exec("qmicli -p -d /dev/cdc-wdm0 --nas-get-rf-band-info")
+        local ab_strip = ab:gsub("\n", "<br>")
+        local ab_title = ab_strip:gsub("%[%/dev%/cdc%-wdm0%] Successfully got RF band info", "<h3>Active Band</h3>")
+        local ab_strip_quotes = ab_title:gsub("'", "")
+        local ab_cleanup = ab_strip_quotes:gsub("Radio Interface\:", "")
+        local ab_s_lte = ab_cleanup:gsub("lte<br>", "")
+        local ab_s_umts = ab_s_lte:gsub("umts<br>", "")
+        local eutran = ab_s_umts:gsub("eutran%-", "LTE Band ")
+        local wcdma = eutran:gsub("wcdma%-", "WCDMA-")
+        local strip_br = wcdma:gsub("<h3>Active Band</h3><br>", "<h3>Active Band</h3>")
+        local active_band = strip_br
+
+        local castatus = luci.sys.exec("qmicli -p -d /dev/cdc-wdm0 --nas-get-lte-cphy-ca-info")
+        local strip = castatus:gsub("\n", "<br>")
+        local ch_title = strip:gsub("%[%/dev%/cdc%-wdm0%] Successfully got carrier aggregation info", "<h3>Mobile Network Information</h3>Carrier aggregation and band information. <br>Note: Secondary carrier aggregation is active only durring network activity. Bandwidth shown in MHz.")
+        local x = ch_title:gsub("Primary Cell Info", "<br><h4>Primary Cell Info</h4>")
+        local y = x:gsub("Secondary Cell Info", "<br><h4>Secondary Cell Info</h4>")
+        local strip_sq = y:gsub("'", "")
+        local strip_eutran = strip_sq:gsub("eutran%-", "")
+        local adv_mobile_info = strip_eutran
+
         local msg_imei = bold .. translate("IMEI") .. sp .. nbold .. imei .. br
         local msg_access_tech = bold .. translate("Access Technology") .. sp .. nbold .. access_tech .. br
         local msg_operator_name = bold .. translate("Operator Name") .. sp ..nbold ..operator_name .. br
-        
+
         -- signal information
         local msg_percent = bold .. translate("Percentage") .. sp .. nbold .. mobile_percentage .."%" .. br
         local msg_rssi = bold .. translate("RSSI") .. sp .. nbold .. mobile_rssi .. br
@@ -58,41 +79,41 @@ if has_mmcli then
         local msg_snr  = bold .. translate("SNR")  .. sp .. nbold .. mobile_snr  .. br
         local msg_rscp  = bold .. translate("RSCP") .. sp .. nbold .. mobile_rscp .. br
         local msg_ecio  = bold .. translate("EC/IO").. sp .. nbold .. mobile_ecio .. br
-	
+
 	local internet_header = "<h3>" .. translate("Internet Connection") .. "</h3>"
-	local minfo_header = "<h3>" .. translate("Modem Details") .. "</h3>"        
+	local minfo_header = "<h3>" .. translate("Modem Details") .. "</h3>"
 	local signal_header = "<h3>" .. translate("Signal Information") .. "</h3>"
 	local mfailed_header = "<h3>" .. translate("Connection Failed") .. "</h3>"
 	local msg_sim_missing = translate("The SIM card could not be detected.  Please check that it is installed correctly and that it is has been activated. <br>Tip: If the SIM card works in a mobile phone, please check whether there is PIN lock on the SIM card.  You can enter a PIN and other SIM authentication details when editing the Mobile Data interface settings in the Advanced Settings section.<br><br><br><a href='https://" .. lanIPNumber .. "/cgi-bin/luci/admin/network/network/mobiledata'>Go to Mobile Data network settings</a>")
 	local msg_no_signal = translate("No signal could be detected.  Please ensure that the antennas are connected, and that suitable signal is available in your area.")
 	local internet_info = internet_header .. internetStatus
 	local modem_info = minfo_header .. msg_access_tech .. msg_operator_name .. msg_imei
-	
+
 	local signal_info = ""
-	
+
 	if mobile_percentage == "0" then
 		signal_info = signal_header .. "" .. msg_no_signal .. "<br>"
 		else
 	if string.match(access_tech, "LTE") then
-	        signal_info = signal_header ..  msg_rssi .. msg_rsrq .. msg_rsrp .. msg_snr .. msg_percent
-	    elseif string.match(access_tech, "3G") then
-	        signal_info = signal_header ..  msg_rssi .. msg_rscp .. msg_ecio .. msg_percent
+	        signal_info = signal_header ..  msg_rssi .. msg_rsrq .. msg_rsrp .. msg_snr .. msg_percent .. active_band .. adv_mobile_info
+	    elseif string.match(access_tech, "UMTS") then
+	        signal_info = signal_header ..  msg_rssi .. msg_rscp .. msg_ecio .. msg_percent .. active_band
 	    elseif string.match(access_tech, "UNKNOWN") then
-		modem_info = minfo_header .. msg_imei
-		signal_info = signal_header .. msg_rssi .. msg_percent
+			modem_info = minfo_header .. msg_imei
+			signal_info = signal_header .. msg_rssi .. msg_percent .. active_band
 		end
 	end
-	
+
 	if string.match(sim_status, "missing") then
 		modem_info = minfo_header .. msg_imei .. "<br><b>SIM card is missing</b><br><p>" .. msg_sim_missing .. "</p><br>"
 		signal_info = ""
 	end
-	
+
 	local msg = ""
 	msg = internet_info .. modem_info .. signal_info
 	msg = msg .. "</p>"
 	m.message = msg
-else 
+else
 
         msg = translate("This device's modem was not detected.  Please reinstall the firmware without keeping settings or reset to factory defaults.  You can perform either of these actions <a href='https://" .. lanIPNumber .. "/cgi-bin/luci/admin/system/flashops'>on this page</a>.")
         m.message = msg
