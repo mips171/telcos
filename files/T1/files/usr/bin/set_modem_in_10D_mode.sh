77e02543441abd519bb7f6e9f902057776272858
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2019-10-03 14:45:29 +1000

    update T1 files

--- files/T1/files/usr/bin/set_modem_in_10D_mode.sh
+++ files/T1/files/usr/bin/set_modem_in_10D_mode.sh
@@ -1,25 +1,35 @@
 #!/bin/sh
 # set_modem_in_10D_mode.sh
 #
-# Sets the modem in 10D mode (1 wwan0 interface) if it is not already in 10D mode.
-# Normal response from Sierra Wireless modem is blank, i.e. response: ''
+# Sets the modem in 10D mode, 000010D (1 wwan0 interface) if it is not already in 10D mode.
+# Modem needs a little extra time after AT!RESET to initialise.
 #
 # Copyright 2019 Telco Antennas Pty Ltd.
 # Nicholas Smith <nicholas.smith@telcoantennas.com.au>
 #
-
 if [ $(cat /etc/modemsettings/modem_in_10D) = "false" ]; then
-  mmcli -m 0 --command="AT!ENTERCND=\"A710\""
-  logger -t INFO "$0: Entered admin mode."
-  sleep 2
-  logger -t INFO "$0: Set modem in 10D mode"
-  mmcli -m 0 --command="AT!USBCOMP=1,1,10D" 
-  sleep 2
-  mmcli -m 0 --command="AT!RESET"
-  logger -t INFO "$0: Resetting modem.  Waiting 30s"
-  echo true > /etc/modemsettings/modem_in_10D
-  sleep 30
-  /etc/init.d/modemmanager restart
+
+	logger -t INFO "Stopping ModemManager service."
+	ifdown mobiledata
+	sleep 5
+	/etc/init.d/modemmanager stop
+	sleep 5
+
+	logger -t INFO "Entering Sierra Wireless admin mode."
+	printf "AT!ENTERCND=\"A710\"\r" > /dev/ttyUSB2
+	sleep 1
+
+	logger -t INFO "Issuing command."
+	printf "AT!USBCOMP=1,1,10D\r" > /dev/ttyUSB2
+	sleep 1
+
+	logger -t INFO "Resetting modem."
+	printf "AT!RESET\r" > /dev/ttyUSB2
+	sleep 15
+	echo "true" > /etc/modemsettings/modem_in_10D
+	logger -t INFO "Restarting ModemManager service."
+	/etc/init.d/modemmanager start
+	
 else
-  logger -t INFO "$0: Modem already in 10D mode."
+	logger -t INFO "Modem already is in 10D mode."
 fi
