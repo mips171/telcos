1db4294719d85ad3f56a3858437c52478402592d
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2020-01-15 13:29:25 +1000

    cleanup files dir

--- files/T1/files/usr/bin/set_imei.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-IMEI=$(mmcli -m 0 |  grep "equipment id" |  tr -cd [:digit:])
-uci set system.@system[0].imei=$IMEI
-uci commit system
-uci commit luci
---
+++ files/T1/usr/bin/refresh_ssl_certificates.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Regenerate SSL keys for HTTPS, set HTTPS server to use new 
+# certificates then restart uhttpd service.
+# Author: Nicholas Smith <nicholas.smith@telcoantennas.com.au>
+# 13-Jan-2020
+
+echo "Regenerating SSL certificates for a validity of 730 days then restarting HTTP server."
+logger -t INFO "Regenerating SSL certificates for a validity of 730 days then restarting HTTP server."
+/usr/bin/openssl req -x509 -days 730 -sha256 -outform der -nodes -keyout /etc/uhttpd.key -out /etc/uhttpd.crt -config=/etc/telcoscert.config
+echo "Restarting web server to use new certificates."
+/etc/init.d/uhttpd restart
+echo "Done."
