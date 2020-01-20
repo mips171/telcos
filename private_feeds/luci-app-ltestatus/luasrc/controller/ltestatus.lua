9c31910f467e1ffd5123485b237d0cac905101e0
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2020-01-20 20:52:00 +1000

    luci-app-ltestatus: update to dynamic version

--- private_feeds/luci-app-ltestatus/luasrc/controller/ltestatus.lua
+++ private_feeds/luci-app-ltestatus/luasrc/controller/ltestatus.lua
@@ -1,6 +1,6 @@
 module("luci.controller.ltestatus", package.seeall)
 
 function index()
-	entry({"admin", "status", "ltestatus"}, form("ltestatus"), _("Mobile Data Status"), 1)
-	entry({"mini", "status", "ltestatus"}, form("ltestatus"), _("Mobile Data Status"), 1)
+	entry({"admin", "status", "ltestatus"}, template("ltestatus"), _("Mobile Data Status"), 1)
+	entry({"mini", "status", "ltestatus"}, template("ltestatus"), _("Mobile Data Status"), 1)
 end
